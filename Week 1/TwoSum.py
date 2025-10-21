5b998cf6e6bf802922e5136c25ebd0d19806b3bd
Nhat Nam <phannam28082004@gmail.com>
2025-10-21 20:51:54 +0700

    2ex_easy

--- Week 1/TwoSum.py
+++ Week 1/TwoSum.py
@@ -1,23 +1,23 @@
-# nums = list(map(int, input('Array:').split()))
-# target = int(input('Target:'))
-# def twosum(nums, target):
-#     seen = {}
-#     for i, num in enumerate(nums):
-#         complement = target - num
-#         if complement in seen:
-#             return [seen[complement], i]
-#         seen[num] = i
-#     return []
-# output = twosum(nums, target)
-# print(output)
-from typing import List
+nums = list(map(int, input('Array:').split()))
+target = int(input('Target:'))
+def twosum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
+output = twosum(nums, target)
+print(output)
+# from typing import List
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         num_map = {}
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in num_map:
+#                 return [num_map[complement], i]
+#             num_map[num] = i
     
